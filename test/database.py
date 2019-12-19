1d63fdeadfbf6dadfb328c439f706d5120bdee2e
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2019-12-19 13:22:52 -0800

    added cat pictures

--- test/database.py
+++ test/database.py
@@ -2,3 +2,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
+
+# add something to the database so that somebody can click the button "favorite" and it will store that image url, id, and breed
+
+# create 4 tables so that there will be table for saving files from random image page, from breed page, from type page, and from category page. 
--- test/home.py
+++ test/home.py
@@ -10,8 +10,31 @@ def test_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
 
     # define home page
-    @app.route('/')
+    @app.route('/', methods=['POST','GET'])
     def home():
-        return render_template('home.html', title="first app!!")
+        return render_template('home.html', title="first app!!", picture=images())
+
+    # function to get random cat images
+    def images():
+        image = requests.get('https://api.thecatapi.com/v1/images/search')
+        cats = image.json()[0]['url']
+        # add something to be able to display the breed of the cat and it's id
+        return cats
+        # add a button to generate random images
+
+    # a function to search for cats by breed
+    @app.route('/breed')
+    def breed():
+        pass
+
+    # a function to search for cats by category
+    @app.route('/category')
+    def category():
+        pass
+
+    # a function to search for cats by file type(gifs or images)
+    @app.route('/type')
+    def file_type():
+        pass
 
     return app

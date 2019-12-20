437d4abfda22d0102ae9a3a52834b65713257b24
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2019-12-19 19:04:56 -0800

    added database schema

--- test/database.py
+++ test/database.py
@@ -5,4 +5,10 @@ DB = SQLAlchemy()
 
 # add something to the database so that somebody can click the button "favorite" and it will store that image url, id, and breed
 
+class fav_cats(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    link = DB.Column(DB.String(150))
+    breed = DB.Column(DB.String(50))
+    breed_id = DB.Column(DB.String(10))
+
 # create 4 tables so that there will be table for saving files from random image page, from breed page, from type page, and from category page. 
--- test/home.py
+++ test/home.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-from .database import DB
+from .database import DB, fav_cats
 
 def test_app():
     # define and instantiate application
@@ -8,6 +8,9 @@ def test_app():
 
     # Setting up/configuring database
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+
+    # how the database knows about the app
+    DB.init_app(app)
 
     # define home page
     @app.route('/', methods=['POST','GET'])
@@ -47,6 +50,29 @@ def test_app():
         breeds = list_breed() # just call the list_breed function
         breed = breeds[cat] # call the id(key) from the list_breed() dictionary which returns the value associated with it as the name of  the cat displayed on the screen.
         return render_template('home.html', title="first app!!", picture=image, breeds = breeds, breed=breed)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return 'Database has been reset'
+
+    # function for saving cat pictures into a database
+    @app.route('/saved_cats')
+    def saved_cats():
+        name_id = request.values['category'] #get the breed id of the cat
+        breeds = list_breed() #dictionary with bree names
+        name = breeds[name_id] # get the breed(value) of the cat from the name_id(key)
+        link = request.values['CAT_URL'] #get the value from this name which is defined in home.htl
+
+        DB.session.add(fav_cats(link=link, breed=name, breed_id=name_id))
+        DB.session.commit()
+        return render_template('home.html', title="first app!!", picture=images(), breeds = breeds, breed=breed)
+
+    @app.route('/favorites')
+    def favorites():
+        faves = fav_cats.query.with_entites(link)
+        return render_template("cat_faves.html", faves=faves. title='favorite cats')
 
 
     # a function to search for cats by category
